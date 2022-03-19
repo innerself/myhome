f48e3ec697d8960ca421200e407db897b531fe5a
Sergey Petrin <spetrin@gmail.com>
2022-03-19 21:50:12 +0300

    Restyle

--- manuals/urls.py
+++ manuals/urls.py
@@ -2,14 +2,10 @@ from django.urls import path
 
 from . import views
 
-
 app_name = 'manuals'
 
-
 urlpatterns = [
-   path('', views.index, name='index'),
-   path('home/', views.home, name='home-index'),
-   path('home/<str:topic>/', views.home_topic, name='home-topic'),
-   path('dacha/', views.dacha, name='dacha-index'),
-   path('dacha/<str:topic>/', views.dacha_topic, name='dacha-topic'),
+    path('', views.index, name='index'),
+    path('home/', views.home, name='home-index'),
+    path('dacha/', views.dacha, name='dacha-index'),
 ]
--- manuals/views.py
+++ manuals/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
 
@@ -10,19 +9,5 @@ def home(request):
     return render(request, 'manuals/home.html')
 
 
-def home_topic(request, topic: str):
-    if topic == 'mi-stick':
-        return render(request, f'manuals/mi-stick.html')
-    else:
-        return HttpResponseNotFound('<h1>Пока такой инструкции нет<h1>')
-
-
 def dacha(request):
     return render(request, 'manuals/dacha.html')
-
-
-def dacha_topic(request, topic: str):
-    if topic == 'generator':
-        return render(request, f'manuals/generator.html')
-    else:
-        return HttpResponseNotFound('<h1>Пока такой инструкции нет<h1>')
